3b5bbe4c48b83b1dfc7f8634778c431d2f41533d
Keropon <daisuke.exe@gmail.com>
2022-12-31 19:53:16 -0300

    :heavy_plus_sign: firestore init

---
+++ src/firebase/init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeSNQhjlhE8A2TPEWsh2q8_hbaddHe6xE",
+  authDomain: "gc-awards-2022-f6f94.firebaseapp.com",
+  projectId: "gc-awards-2022-f6f94",
+  storageBucket: "gc-awards-2022-f6f94.appspot.com",
+  messagingSenderId: "459061574908",
+  appId: "1:459061574908:web:7fb2985267be4706f0ab79"
+};
+
+const app = initializeApp(firebaseConfig);
